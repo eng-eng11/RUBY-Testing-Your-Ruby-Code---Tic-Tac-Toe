e63f99e656ae2eac2e08d030775b7f73d4278c78
stickler-ci <support@stickler-ci.com>
2019-10-23 19:27:26 +0000

    Fixing style errors.

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require './game.rb'
 
 describe Game do
-  subject(:game){Game.new()}
+  subject(:game) { Game.new }
 
   describe '#switch_player' do
-    it "changes current_player to the other player" do
+    it 'changes current_player to the other player' do
       player = game.current_player
       game.switch_player
       expect(game.current_player).not_to eq(player)
@@ -12,29 +14,28 @@ describe Game do
   end
 
   describe '#over?' do
-    it "returns true when a win condition is met" do
-      for i in 1..3 do
+    it 'returns true when a win condition is met' do
+      (1..3).each do |i|
         game.player_1.add_cell(i)
       end
       expect(game.over?).to be true
     end
   end
-
 end
 
 describe Board do
-  subject(:board){Board.new()}
+  subject(:board) { Board.new }
 
   describe '#update_field_values' do
     it "add's a chosen location to the list of spaces taken" do
       board.update_field_values('x', 5)
-      expect(board.board_values).to eq([" ", " ", " ", " ", "x", " ", " ", " ", " "])
+      expect(board.board_values).to eq([' ', ' ', ' ', ' ', 'x', ' ', ' ', ' ', ' '])
     end
   end
 end
 
 describe Player do
-  subject(:player){Player.new('Ron', 'x')}
+  subject(:player) { Player.new('Ron', 'x') }
 
   describe '#add_cell' do
     it "updates the player's list of cells taken" do
