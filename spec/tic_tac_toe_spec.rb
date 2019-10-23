415f4374ffa15f446bf267cd06639dacc99f5ab3
davidauza.engineer <davidauza01@gmail.com>
2019-10-23 14:27:51 -0500

    update tic_tac_toe_spec.rb

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,16 +1,10 @@
-# frozen_string_literal: true
-
 require './game.rb'
 
 describe Game do
-<<<<<<< HEAD
-  subject(:game) { Game.new() }
-=======
-  subject(:game) { Game.new }
->>>>>>> 4eee0904edacbc7d585849f94d935e39517acf1e
+  subject(:game){Game.new()}
 
   describe '#switch_player' do
-    it 'changes current_player to the other player' do
+    it "changes current_player to the other player" do
       player = game.current_player
       game.switch_player
       expect(game.current_player).not_to eq(player)
@@ -18,36 +12,29 @@ describe Game do
   end
 
   describe '#over?' do
-    it 'returns true when a win condition is met' do
-      (1..3).each do |i|
+    it "returns true when a win condition is met" do
+      for i in 1..3 do
         game.player_1.add_cell(i)
       end
       expect(game.over?).to be true
     end
   end
+
 end
 
 describe Board do
-<<<<<<< HEAD
-  subject(:board) { Board.new() }
-=======
-  subject(:board) { Board.new }
->>>>>>> 4eee0904edacbc7d585849f94d935e39517acf1e
+  subject(:board){Board.new()}
 
   describe '#update_field_values' do
     it "add's a chosen location to the list of spaces taken" do
       board.update_field_values('x', 5)
-      expect(board.board_values).to eq([' ', ' ', ' ', ' ', 'x', ' ', ' ', ' ', ' '])
+      expect(board.board_values).to eq([" ", " ", " ", " ", "x", " ", " ", " ", " "])
     end
   end
 end
 
 describe Player do
-<<<<<<< HEAD
-  subject(:player) { Player.new('Ron', 'x') }
-=======
-  subject (:player) { Player.new('Ron', 'x') }
->>>>>>> 4eee0904edacbc7d585849f94d935e39517acf1e
+  subject(:player){Player.new('Ron', 'x')}
 
   describe '#add_cell' do
     it "updates the player's list of cells taken" do
