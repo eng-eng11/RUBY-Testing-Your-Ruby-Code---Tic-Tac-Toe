5eb5f4ac4cf28b6f607486380ad65f518eeef368
davidauza.engineer <davidauza01@gmail.com>
2019-10-24 14:09:59 -0500

    Update tests for board.rb

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 require './board.rb'
-# require './game.rb'
+require './game.rb'
 
-# tests for the class Board
+# tests for the class board
 describe Board do
   describe '#draw_board' do
     let(:board) { Board.new }
@@ -15,7 +15,7 @@ describe Board do
     end
   end
 
-  describe 'update_field_values' do
+  describe '#update_field_values' do
     let(:board) { Board.new }
     it 'update field values properly' do
       expect(board.update_field_values('X', 3)).to eq('X')
@@ -23,45 +23,45 @@ describe Board do
   end
 end
 
-# describe Game do
-#   subject(:game) { Game.new }
-#
-#   describe '#switch_player' do
-#     it 'changes current_player to the other player' do
-#       player = game.current_player
-#       game.switch_player
-#       expect(game.current_player).not_to eq(player)
-#     end
-#   end
-#
-#   describe '#over?' do
-#     it 'returns true when a win condition is met' do
-#       (1..3).each do |i|
-#         game.player_one.add_cell(i)
-#       end
-#       expect(game.over?).to be true
-#     end
-#   end
-# end
-#
-# describe Board do
-#   subject(:board) { Board.new }
-#
-#   describe '#update_field_values' do
-#     it "add's a chosen location to the list of spaces taken" do
-#       board.update_field_values('x', 5)
-#       expect(board.board_values).to eq([' ', ' ', ' ', ' ', 'x', ' ', ' ', ' ', ' '])
-#     end
-#   end
-# end
-#
-# describe Player do
-#   subject(:player) { Player.new('Ron', 'x') }
-#
-#   describe '#add_cell' do
-#     it "updates the player's list of cells taken" do
-#       player.add_cell(3)
-#       expect(player.cells_chosen).to eq([3])
-#     end
-#   end
-# end
+describe Game do
+  subject(:game) { Game.new }
+
+  describe '#switch_player' do
+    it 'changes current_player to the other player' do
+      player = game.current_player
+      game.switch_player
+      expect(game.current_player).not_to eq(player)
+    end
+  end
+
+  describe '#over?' do
+    it 'returns true when a win condition is met' do
+      (1..3).each do |i|
+        game.player_one.add_cell(i)
+      end
+      expect(game.over?).to be true
+    end
+  end
+end
+
+describe Board do
+  subject(:board) { Board.new }
+
+  describe '#update_field_values' do
+    it "add's a chosen location to the list of spaces taken" do
+      board.update_field_values('x', 5)
+      expect(board.board_values).to eq([' ', ' ', ' ', ' ', 'x', ' ', ' ', ' ', ' '])
+    end
+  end
+end
+
+describe Player do
+  subject(:player) { Player.new('Ron', 'x') }
+
+  describe '#add_cell' do
+    it "updates the player's list of cells taken" do
+      player.add_cell(3)
+      expect(player.cells_chosen).to eq([3])
+    end
+  end
+end
